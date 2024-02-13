e328a47a780e28f3cc36d262a2037fbd7122b7c7
Fabian Ehis <fabian.ehis@student.vives.be>
2024-02-13 19:20:46 +0100

    Introductiepagina en register pagina aangemaakt

---
+++ E_commerce_Voetbal/E_commerce_Voetbal/Controllers/IntroductionController.cs
@@ -0,0 +1,43 @@
+﻿using E_commerce_Voetbal.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce_Voetbal.Controllers
+{
+    public class IntroductionController : Controller
+    {
+
+        /**
+         * Introductiepagina
+         */
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /**
+         * Registreerpagina
+         */
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        /**
+         * Registreerpagina
+         */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterVM registerVM)
+        {
+           if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(registerVM);
+        }
+    }
+}
